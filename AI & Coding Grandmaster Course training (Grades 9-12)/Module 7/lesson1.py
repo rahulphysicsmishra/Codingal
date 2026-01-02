8fb1fe486963b84450e2ee58927fb6c9be74e205
rahul mishra <rahulphysicsmishra@gmail.com>
2026-01-02 16:32:39 +0530

    module 7 lesson 1

---
+++ AI & Coding Grandmaster Course training (Grades 9-12)/Module 7/lesson1.py
@@ -0,0 +1,16 @@
+# activity 1
+
+print("Hello, World!")
+
+# activity 2
+
+def greet(name):
+    return "Hello, " + name + "!"
+
+print(greet("Alice"))
+
+#activity 3
+
+import keyword
+
+print(keyword.kwlist)
