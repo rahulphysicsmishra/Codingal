0294e5b2fcf4876e41fd5254270f19b7dab68494
rahul mishra <rahulphysicsmishra@gmail.com>
2026-07-11 05:22:05 +0530

    m23,lesson5

---
+++ AI & Coding Grandmaster Course training (Grades 9-12)/Module 23/lesson 5/act4.java
@@ -0,0 +1,13 @@
+class Main {
+
+    int age;
+    Main(int age){
+        age = age;
+		//this.age = age;
+    }
+
+    public static void main(String[] args) {
+        Main obj = new Main(24);
+        System.out.println("obj.age = " + obj.age);
+    }
+}
